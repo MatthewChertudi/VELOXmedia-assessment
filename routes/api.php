85a229900d1d9e282246ec151315a516251e0c19
Matthew Chertudi <matthewchertudi@Matthews-Mac-mini.local>
2024-09-17 22:23:17 -0600

    add route for GET /articles

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
 Route::post('/articles', [ArticleController::class, 'store']);
+
+Route::get('/articles', [ArticleController::class, 'index']);
