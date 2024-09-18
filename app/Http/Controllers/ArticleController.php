7e6acd5b1d93e2ca9d427253923062391ade6547
Matthew Chertudi <matthewchertudi@Matthews-Mac-mini.local>
2024-09-17 21:50:10 -0600

    partial store function for post request

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $validateData = $request->validate([
+            'title' => 'required|max:30',
+            'content' => 'required',
+            'author' => 'required',
+            'category' => 'required',
+            'published_at' => 'required',
+        ]);
+    
+        $article = Article::create($validateData);
+
+    }
 }
